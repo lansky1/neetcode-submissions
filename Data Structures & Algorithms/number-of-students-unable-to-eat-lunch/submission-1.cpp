d2053e466f70623af377b2f4ba028c4be641cd96
Ekalavya <77741579+lansky1@users.noreply.github.com>
2026-06-25 03:35:31 +0530

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.cpp
@@ -0,0 +1,27 @@
+#include <deque>
+using namespace std;
+
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        deque<int> sandwich_queue(sandwiches.begin(), sandwiches.end()); 
+        deque<int> student_queue(students.begin(), students.end()); 
+        int distance_of_unhappy_student = -1;
+
+        while (student_queue.size() and distance_of_unhappy_student != student_queue.size()) {
+            if (student_queue[0] == sandwich_queue[0]) {
+                student_queue.pop_front();
+                sandwich_queue.pop_front();
+                distance_of_unhappy_student = -1;
+            }
+            else {
+                int front = student_queue.front();  
+                student_queue.pop_front();          
+                student_queue.push_back(front);
+                distance_of_unhappy_student++;
+            }
+        }
+
+        return student_queue.size();
+    }
+};
