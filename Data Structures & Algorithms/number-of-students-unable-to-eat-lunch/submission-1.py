e9d994ad20de6cd82d8b432b5e78248e6415652a
Ekalavya <77741579+lansky1@users.noreply.github.com>
2026-09-12 01:02:29 +0530

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.py
@@ -0,0 +1,22 @@
+# Attempt 2
+# We dont need sandwich list as a queue
+
+from collections import deque
+
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        skipped_students = 0
+        student_queue = deque(students)
+        sandwich_index = 0
+
+        while skipped_students != len(student_queue):
+            if student_queue[0] == sandwiches[sandwich_index]:
+                skipped_students = 0
+                student_queue.popleft()
+                sandwich_index+=1
+            else:
+                skipped_students += 1
+                student_queue.append(student_queue.popleft())
+
+        return len(student_queue)
