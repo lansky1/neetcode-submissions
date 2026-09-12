551e958600d277478b9bd473505bfb9cec909e2f
Ekalavya <77741579+lansky1@users.noreply.github.com>
2026-09-13 01:11:31 +0530

    Add: insertionSort - submission-1

---
+++ Data Structures & Algorithms/insertionSort/submission-1.py
@@ -0,0 +1,21 @@
+# I think we can save some time, if we dont swap everytime, and save it
+
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        if not pairs:
+            return pairs
+
+        stepList = [pairs.copy()]
+        for i in range(1, len(pairs)):
+            j = i-1
+            while(j >= 0 and pairs[j].key>pairs[j+1].key):
+                pairs[j+1], pairs[j] = pairs[j], pairs[j+1]
+                j-=1
+            stepList.append(pairs.copy())
+            
+        return stepList
