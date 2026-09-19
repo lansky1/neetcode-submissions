3fdff6c9cf27254b9ffca2bce16ff54bfb3b4f3c
Ekalavya <77741579+lansky1@users.noreply.github.com>
2026-09-20 00:49:00 +0530

    Add: k-closest-points-to-origin - submission-4

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        self._quick_sort(points, 0, len(points)-1, k)
+        return points[:k]
+    
+    def _quick_sort(self, points, start, end, k):
+        if start>=end:
+            return
+
+        swap_index = start
+        for i in range(start, end):
+            if self._calculate_distance(points[i]) <= self._calculate_distance(points[end]):
+                points[swap_index], points[i] = points[i], points[swap_index]
+                swap_index += 1
+
+        points[swap_index], points[end] = points[end], points[swap_index]
+
+        if k == swap_index+1:
+            return
+        elif k > swap_index+1:
+            self._quick_sort(points, swap_index + 1, end, k)
+        else:
+            self._quick_sort(points, start, swap_index - 1, k)
+
+    def _calculate_distance(self, point):
+        return pow((pow(point[0], 2) + pow(point[1], 2)), 0.5)
+
+        
